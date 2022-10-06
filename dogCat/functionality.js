ebb9477ae4d8207b405b165a974b3eecbfbd802f
alessandroTHFC <apangraz@SRA-LT-2033.sra.com.au>
2022-10-06 21:17:55 +1030

    new colour scheme/landing 30%

--- dogCat/functionality.js
+++ dogCat/functionality.js
@@ -1,15 +1,31 @@
 profPic = localStorage.getItem("animalPic")
+userName = localStorage.getItem("name");
 
 function changeAnimalImg(value) {
     profPic = localStorage.setItem("animalPic", value);
 }
 
 function setProfileImage() {
+    text = document.getElementById("userAnimalPreference");
     image = document.getElementById("animalImg");
-    if (profPic == 'cat')
+    if (profPic == 'cat') {
+        text.textContent = userName + " is a Cat Lover"
         image.src = "catImg.png";
-    else if (profPic == 'dog')
+    }
+    else if (profPic == 'dog'){
+        text.textContent = userName + " is a Dog Lover"
         image.src = "dogImg.png";
+    }
+    setUsernameToSpan();
+        
+}
+
+function    getUsername(username) {
+    userName = localStorage.setItem("name", username);
+}
+
+function    setUsernameToSpan() {
+    document.getElementById("clientName").textContent = userName; 
 }
 
 function popOutMenu() {
